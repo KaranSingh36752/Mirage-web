24ff7e01530c8440b583756c6ff9dc1c9a12a9d8
karan <karansingh36752@gmail.com>
2024-11-04 23:30:58 +0530

    ROUTER

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import Body from './components/Body';
+import Login from './components/Login';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
 function App() {
+    const appRouter = createBrowserRouter([
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/login",
+        element: <Login/>
+      }
+    ])
+
   return (
-      <Body/>
+      <div>
+        <RouterProvider router={appRouter} />
+      </div>
   );
 }
 
